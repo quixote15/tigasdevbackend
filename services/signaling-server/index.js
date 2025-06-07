50891ddd3fc477db83368de9416a95315af4cde1
tiagosantos-ca <tiago.santos@contaazul.com>
2025-06-07 20:56:31 -0300

    fix: add cors

--- services/signaling-server/index.js
+++ services/signaling-server/index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 // Setup Socket.IO with CORS configuration
 const io = new Server(server, {
     cors: {
-        origin: ["https://tigasdev.com"],
+        origin: ["https://tigasdev.com", '*'],
         methods: ["GET", "POST"],
         credentials: false
     },
